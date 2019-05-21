0e9724d4f610c9e61bae0cd34f252ae21e3d4928
radnotiz <2194747+radnotiz@users.noreply.github.com>
2019-05-21 22:40:12 +0200

    Add converting three digits numbers

--- convert.js
+++ convert.js
@@ -1,21 +1,37 @@
 const wordsUpToNineteen = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eigth", "nine",
-        "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+    "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
 
 const wordsOfTens = ["zero", "ten", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"];
 
-
-function convert(number) {
+function convertTwoDigits(number) {
     if (number < 20) {
-        return wordsUpToNineteen[number];
-    } else {
+        return [wordsUpToNineteen[number]];
+    } else if (number < 100) {
         const tens = Math.floor(number / 10);
         const rest = number - (10 * tens);
         const result = [wordsOfTens[tens]];
         if (rest > 0) {
             result.push(wordsUpToNineteen[rest]);
         }
-        return result.join(' ');
+        return result;
     }
+}
+
+function convert(number) {
+    const result = [];
+    if (number < 100) {
+        result.push(...convertTwoDigits(number));
+    } else {
+        const hundreds = Math.floor(number / 100);
+        const rest = number - (100 * hundreds);
+        result.push(wordsUpToNineteen[hundreds]);
+        result.push('hundred');
+        if (rest > 0) {
+            result.push('and');
+            result.push(...convertTwoDigits(rest));
+        }
+    }
+    return result.join(' ');
 }
 
 module.exports = convert;
--- convert.test.js
+++ convert.test.js
@@ -1,53 +1,25 @@
 const convert = require('./convert');
 
-test('converts 1', ()=>{
-    expect(convert(1)).toBe('one');
-});
-
-test('converts 5', ()=>{
-    expect(convert(5)).toBe('five');
-});
-
-test('converts 9', ()=>{
-    expect(convert(9)).toBe('nine');
-});
-
-test('converts 0', ()=>{
-    expect(convert(0)).toBe('zero');
-});
-
-test('converts 10', ()=>{
-    expect(convert(10)).toBe('ten');
-});
-
-test('converts 11', ()=>{
-    expect(convert(11)).toBe('eleven');
-});
-
-test('converts 12', ()=>{
-    expect(convert(12)).toBe('twelve');
-});
-
-test('converts 13', ()=>{
-    expect(convert(13)).toBe('thirteen');
-});
-
-test('converts 19', ()=>{
-    expect(convert(19)).toBe('nineteen');
-});
-
-test('converts 20', ()=>{
-    expect(convert(20)).toBe('twenty');
-});
-
-test('converts 90', ()=>{
-    expect(convert(90)).toBe('ninety');
-});
-
-test('converts 43', ()=>{
-    expect(convert(43)).toBe('fourty three');
-});
-
-test('converts 99', ()=>{
-    expect(convert(99)).toBe('ninety nine');
+test.each([
+    [1, "one"],
+    [5, "five"],
+    [9, "nine"],
+    [0, "zero"],
+    [10, "ten"],
+    [11, "eleven"],
+    [12, "twelve"],
+    [13, "thirteen"],
+    [19, "nineteen"],
+    [20, "twenty"],
+    [90, "ninety"],
+    [43, "fourty three"],
+    [99, "ninety nine"],
+    [100, "one hundred"],
+    [134, "one hundred and thirty four"],
+    [500, "five hundred"],
+    [501, "five hundred and one"],
+    [520, "five hundred and twenty"],
+    [999, "nine hundred and ninety nine"]
+])('converts %i', (number, expected) => {
+    expect(convert(number)).toBe(expected);
 });
